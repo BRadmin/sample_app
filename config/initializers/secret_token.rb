40a00e10b74b93bd91218840c2073d72e22b4cf5
BRadmin <bradmin@paperdrop.com>
2010-10-28 14:45:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6c526eb04a26bf03dea5a008dc40c1148ce89f14636a04bff1855861a73eef1fa6cd1cccc99dde5df07ef36fe3e8025ef9e5c2e6ed47738583ec9659232acbc1'
